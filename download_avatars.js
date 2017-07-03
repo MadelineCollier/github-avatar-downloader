130ab603c9d47bf9da34c0763c1db7937f4ed3fc
Madeline Collier <madel@telus.net>
2017-07-03 23:37:58 +0000

    added github username and tokens, but did it in a slighthly modified way from the instructions so as to avoid publishing token on public repo

--- download_avatars.js
+++ download_avatars.js
@@ -2,12 +2,13 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+const user = process.env.GITHUB_USER;
+const token = process.env.GITHUB_ACCESS_TOKEN;
+
 function getRepoContributors(repoOwner, repoName, callback) {
-
+  var requestURL = 'https://' + user + ':' + token + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
-
-
-
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
