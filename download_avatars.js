b70e09c26a47ad167ee19ac79334599a4e85dcb1
Madeline Collier <madel@telus.net>
2017-07-04 01:43:27 +0000

    final linting check

--- download_avatars.js
+++ download_avatars.js
@@ -14,29 +14,28 @@ function getRepoContributors(repoOwner, repoName, callback) {
   if (repoOwner === 'undefined') {
     console.log('Name of repository owner REQUIRED');
     return;
-  }
+  };
   if (repoName === 'undefined') {
     console.log('Name of repository REQUIRED');
     return;
-  }
+  };
   var requestURL = 'https://' + process.env.GITHUB_USER + ':' + process.env.GITHUB_ACCESS_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   var requestOption = {
     headers: {
       'User-Agent': "GitHub Avatar Downloader - Student Project"
     }
-  }
+  };
   request.get(requestURL, requestOption, function(error, response, body) {
         var data = JSON.parse(body);
         callback(data);
   });
-
-}
+};
 
 //defining the function which is passed into findAvatar:
 function downloadImageByURL(url, filePath) {
   request.get(url)
          .pipe(fs.createWriteStream(filePath));
-}
+};
 
 //defining the callback function that is passed to getRepoContributors:
 function findAvatar(someJSONthing) {
